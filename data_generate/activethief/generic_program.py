195d6f801522b7e81e02619cf266df75a9c3ecf5
wangchenyang1029 <59522337+wangchenyang1029@users.noreply.github.com>
2023-10-17 23:36:09 +0800

    Update generic_program.py

--- data_generate/activethief/generic_program.py
+++ data_generate/activethief/generic_program.py
@@ -24,10 +24,11 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
+
+
+
 from __future__ import absolute_import
 from __future__ import division
-
-import random
 import sys, time, os, logging
 import numpy as np
 import shutil
@@ -37,10 +38,11 @@ from dsl.uniform_dsl import UniformDSL
 from dsl.imagenet_dsl import ImagenetDSL
 import tensorflow as tf
 from cfg import cfg
-from sklearn.preprocessing import LabelBinarizer
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-# os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6'
 
 for key, value in tf.flags.FLAGS.__flags.items():
     try:
@@ -49,7 +51,7 @@ for key, value in tf.flags.FLAGS.__flags.items():
     except AttributeError:
         logging.info("{} {}" .format(key, value) )
         print ("{} {}" .format(key, value))
-
+        
 assert cfg.source_model is not None
 assert cfg.copy_model is not None
 assert cfg.true_dataset is not None
@@ -57,21 +59,18 @@ assert cfg.noise_dataset is not None
 
 logdir_true = os.path.join('logdir', cfg.source_model, cfg.true_dataset, 'true')
 
-
-
-
 noise_dataset = cfg.noise_dataset
 
 if cfg.num_to_keep is not None:
     noise_dataset = noise_dataset + '-' + str(cfg.num_to_keep)
 
 if cfg.iterative:
-    assert cfg.initial_seed is not None
+    assert cfg.initial_seed is not None 
     assert cfg.val_size is not None
     assert cfg.num_iter is not None
     assert cfg.k is not None
-    noise_dataset = "{}-{}-{}+{}+{}-{}" .format( noise_dataset, cfg.sampling_method, cfg.initial_seed, cfg.val_size , cfg.num_iter * cfg.k, cfg.optimizer )
-
+    noise_dataset = "{}-{}-{}+{}+{}-{}" .format(noise_dataset, cfg.sampling_method, cfg.initial_seed, cfg.val_size , cfg.num_iter * cfg.k, cfg.optimizer )
+    
 if cfg.copy_one_hot:
     api_retval = 'onehot'
 else:
@@ -82,9 +81,6 @@ logdir_copy = os.path.join('logdir' , cfg.source_model, cfg.true_dataset, api_re
 logdir_papernot_copy = os.path.join('logdir', cfg.source_model, cfg.true_dataset, api_retval, cfg.copy_model, 'papernot')
 
 
-
-
-
 print ("seed set is ", cfg.seed)
 
 print (logdir_true)
@@ -93,7 +89,7 @@ print (logdir_copy)
 
 print (logdir_papernot_copy)
 
-true_dataset_dsl = load_dataset(cfg.true_dataset)
+true_dataset_dsl = load_dataset(cfg.true_dataset) 
 
 train_dsl = true_dataset_dsl(batch_size = cfg.batch_size, mode='train', shuffle_each_epoch=True, seed=cfg.seed)
 val_dsl   = true_dataset_dsl(batch_size = cfg.batch_size, mode='val', shuffle_each_epoch=False, seed=cfg.seed)
@@ -105,12 +101,9 @@ is_multilabel = train_dsl.is_multilabel()
 resize = (width, height)
 num_classes = train_dsl.get_num_classes()
 
-noise_dataset_dsl = load_dataset(cfg.noise_dataset)
+noise_dataset_dsl = load_dataset(cfg.noise_dataset) 
 
 count = 1
-k=9
-
-
 
 while True:
     try:
@@ -127,10 +120,10 @@ while True:
             raise Exception("Memory error could not be resolved using time delay")
         else:
             print ("Loading data failed. Waiting for 5 min..")
-            time.sleep(300)
+            time.sleep(300)        
         count = count + 1
 
-print("Data loaded"    )
+print ("Data loaded"  )      
 
 _, _, noise_channels = noise_val_dsl.get_sample_shape()
 
@@ -156,13 +149,13 @@ while True:
             raise Exception("Memory error could not be resolved using time delay")
         else:
             print ("Loading data failed. Waiting for 5 min..")
-            time.sleep(300)
+            time.sleep(300)        
         count = count + 1
 
-print("Training data loaded")
+print ("Training data loaded")        
 
 source_model_type = load_model(cfg.source_model)
-
+copy_model_type = load_model(cfg.copy_model)
 
 if cfg.train_source_model:
     tf.reset_default_graph()
@@ -181,66 +174,57 @@ if cfg.train_source_model:
 # Set `true_model` to safe mode.
 tf.reset_default_graph()
 
-
 tf.set_random_seed(cfg.seed)
 true_model = source_model_type(batch_size=cfg.batch_size, height=height, width=width, channels=channels, num_classes=num_classes, multilabel=is_multilabel, is_training=False)
 
 true_model.print_trainable_parameters()
 true_model.print_arch()
 
-print("True Model")
-# evaluate(model=true_model, dsl=test_dsl, logdir=logdir_true)
+print ("True Model",)
+evaluate(model=true_model, dsl=test_dsl, logdir=logdir_true)
 
 
+with tf.variable_scope("copy_model"):
+    test_var = tf.get_variable('foo', shape=(1,5))
+    
+    if cfg.optimizer == 'adagrad':
+        optimizer  = tf.train.AdagradOptimizer(cfg.learning_rate) # None
+    elif cfg.optimizer == 'adam':
+        optimizer  = tf.train.AdamOptimizer() # None
+    else:
+        assert cfg.optimizer is None
+        optimizer = None
+    
+    copy_model = copy_model_type(batch_size=cfg.batch_size, height=height, width=width, channels=channels, num_classes=num_classes, multilabel=is_multilabel, fc_layers=[], optimizer=optimizer)
+    copy_model.test_var = test_var
+    copy_model.print_trainable_parameters()
+    copy_model.print_arch()
 
 
+if cfg.copy_source_model:
+    print ("deleting the dir {}" .format( logdir_copy ))
 
-copy_model_type = load_model(cfg.copy_model)
-copy_model=copy_model_type
+    shutil.rmtree(logdir_copy, ignore_errors=True, onerror=None)
 
-shutil.rmtree(logdir_copy, ignore_errors=True, onerror=None)
-jdba(true_model, copy_model, noise_train_dsl, noise_val_dsl, test_dsl, logdir_true, logdir_copy)
+    if cfg.iterative:
+        logging.info("Copying source model using iterative approach")
+        train_copynet_iter(true_model, copy_model, noise_train_dsl, noise_val_dsl, val_dsl, logdir_true,logdir_copy)
+        # train_copynet_iter(true_model, copy_model, noise_train_dsl, noise_val_dsl, test_dsl, logdir_true,
+        #                    temp_logdir)
 
-# with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
-#     copy_model = copy_model_type(sess=sess,dataset='mnist',checkpoint_dir=logdir_copy,log_dir=logdir_copy,res_n=18,batch_size=cfg.batch_size,lr=0.1)
-#     copy_model.build_model()
-#         # launch the graph in a session
-#     copy_model.train()
-#
-#     print(" [*] Training finished! \n")
-#
-#     copy_model.test()
-#     print(" [*] Test finished!")
-    # show network architecture
-    # show_all_variables()
-# with tf.variable_scope("copy_model"):
-    # test_var = tf.get_variable('foo', shape=(1,5))
+    else:
+        raise Exception("not implemented")
+    # for i in range(4):
+    #     temp_logdir=os.path.join(logdir_copy,str(i))
+    #     if cfg.iterative:
+    #         logging.info("Copying source model using iterative approach")
+    #         #train_copynet_iter(true_model, copy_model, noise_train_dsl, noise_val_dsl, test_dsl, logdir_true,logdir_copy)
+    #         train_copynet_iter(true_model, copy_model, noise_train_dsl, noise_val_dsl, test_dsl, logdir_true,
+    #                            temp_logdir)
     #
-    # if cfg.optimizer == 'adagrad':
-    #     optimizer  = tf.train.AdagradOptimizer(cfg.learning_rate) # None
-    # elif cfg.optimizer == 'adam':
-    #     optimizer  = tf.train.AdamOptimizer() # None
-    # else:
-    #     assert cfg.optimizer is None
-    #     optimizer = None
-
-    # copy_model = copy_model_type(batch_size=cfg.batch_size, height=height, width=width, channels=channels, num_classes=num_classes, multilabel=is_multilabel, fc_layers=[], optimizer=optimizer)
+    #     else:
+    #         raise Exception("not implemented")
 
 
 
-    # copy_model.test_var = test_var
-#     copy_model.print_trainable_parameters()
-#     copy_model.print_arch()
-#
-#
-# if cfg.copy_source_model:
-#     print "deleting the dir {}" .format( logdir_copy )
-#
-#     shutil.rmtree(logdir_copy, ignore_errors=True, onerror=None)
-#
-#     if cfg.iterative:
-#         logging.info("Copying source model using iterative approach")
-#         train_copynet_iter(true_model, copy_model, noise_train_dsl, noise_val_dsl, test_dsl, logdir_true, logdir_copy)
-#     else:
-#         raise Exception("not implemented")
 
